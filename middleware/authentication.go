63c452b3252cb66fe400c135b9498fea671bb379
saeed <saeed.afshari8@gmail.com>
2019-12-16 00:33:33 +0100

    Refactoring: extract handler functions

--- middleware/authentication.go
+++ middleware/authentication.go
@@ -38,6 +38,95 @@ type HttpStatusResponse struct {
 
 var JwtAuthentication = func(authInfo *AuthInfo, router *gin.Engine) *jwt.GinJWTMiddleware {
 
+	setDefaults(authInfo)
+
+	// the jwt middleware
+	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+		Realm:           (*authInfo).Realm,
+		Key:             []byte((*authInfo).Secret),
+		Timeout:         (*authInfo).TokenTimeout,
+		MaxRefresh:      (*authInfo).MaxRefresh,
+		IdentityKey:     (*authInfo).IdentityKey,
+		PayloadFunc:     payloadFunc(authInfo),
+		IdentityHandler: identityHandler(authInfo),
+		Authenticator:   authenticator(authInfo),
+		Authorizator:    authorizator(authInfo),
+		Unauthorized:    unauthorizedHandler(),
+		TokenLookup:     "header: Authorization, query: token, cookie: jwt",
+		TokenHeadName:   "Bearer",
+		TimeFunc:        time.Now,
+	})
+
+	if err != nil {
+		log.Fatal("JWT Error:" + err.Error())
+	}
+
+	router.POST((*authInfo).LoginEndPoint, authMiddleware.LoginHandler)
+
+	return authMiddleware
+}
+
+func unauthorizedHandler() func(c *gin.Context, code int, message string) {
+	return func(c *gin.Context, code int, message string) {
+		c.JSON(code, HttpStatusResponse{
+			Code:    fmt.Sprintf("%d", code),
+			Message: message,
+		})
+	}
+}
+
+func authorizator(authInfo *AuthInfo) func(data interface{}, c *gin.Context) bool {
+	return func(data interface{}, c *gin.Context) bool {
+		if account, ok := data.(*Account); ok {
+			if (*authInfo).Authorizator != nil {
+				authorized, err := (*authInfo).Authorizator(account)
+				return err == nil && authorized
+			}
+			return true
+		}
+		return false
+	}
+}
+
+func authenticator(authInfo *AuthInfo) func(c *gin.Context) (interface{}, error) {
+	return func(c *gin.Context) (interface{}, error) {
+		var login Login
+		if err := c.ShouldBind(&login); err != nil {
+			return "", jwt.ErrMissingLoginValues
+		}
+		account, err := (*authInfo).Authenticator(&login)
+		if err != nil {
+			return nil, jwt.ErrFailedAuthentication
+		}
+		return account, nil
+	}
+}
+
+func identityHandler(authInfo *AuthInfo) func(c *gin.Context) interface{} {
+	return func(c *gin.Context) interface{} {
+		claims := jwt.ExtractClaims(c)
+		return &Account{
+			Email:     claims[(*authInfo).IdentityKey].(string),
+			FirstName: claims["firstName"].(string),
+			LastName:  claims["lastName"].(string),
+		}
+	}
+}
+
+func payloadFunc(authInfo *AuthInfo) func(data interface{}) jwt.MapClaims {
+	return func(data interface{}) jwt.MapClaims {
+		if account, ok := data.(*Account); ok {
+			return jwt.MapClaims{
+				(*authInfo).IdentityKey: account.Email,
+				"firstName":             account.FirstName,
+				"lastName":              account.LastName,
+			}
+		}
+		return jwt.MapClaims{}
+	}
+}
+
+func setDefaults(authInfo *AuthInfo) {
 	if (*authInfo).TokenTimeout == 0 {
 		(*authInfo).TokenTimeout = time.Hour
 	}
@@ -52,69 +141,4 @@ var JwtAuthentication = func(authInfo *AuthInfo, router *gin.Engine) *jwt.GinJWT
 	if (*authInfo).Authenticator == nil {
 		panic(errors.New("authenticator is not provided"))
 	}
-
-	// the jwt middleware
-	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
-		Realm:       (*authInfo).Realm,
-		Key:         []byte((*authInfo).Secret),
-		Timeout:     (*authInfo).TokenTimeout,
-		MaxRefresh:  (*authInfo).MaxRefresh,
-		IdentityKey: (*authInfo).IdentityKey,
-		PayloadFunc: func(data interface{}) jwt.MapClaims {
-			if account, ok := data.(*Account); ok {
-				return jwt.MapClaims{
-					(*authInfo).IdentityKey: account.Email,
-					"firstName":             account.FirstName,
-					"lastName":              account.LastName,
-				}
-			}
-			return jwt.MapClaims{}
-		},
-		IdentityHandler: func(c *gin.Context) interface{} {
-			claims := jwt.ExtractClaims(c)
-			return &Account{
-				Email:     claims[(*authInfo).IdentityKey].(string),
-				FirstName: claims["firstName"].(string),
-				LastName:  claims["lastName"].(string),
-			}
-		},
-		Authenticator: func(c *gin.Context) (interface{}, error) {
-			var login Login
-			if err := c.ShouldBind(&login); err != nil {
-				return "", jwt.ErrMissingLoginValues
-			}
-			account, err := (*authInfo).Authenticator(&login)
-			if err != nil {
-				return nil, jwt.ErrFailedAuthentication
-			}
-			return account, nil
-		},
-		Authorizator: func(data interface{}, c *gin.Context) bool {
-			if account, ok := data.(*Account); ok {
-				if (*authInfo).Authorizator != nil {
-					authorized, err := (*authInfo).Authorizator(account)
-					return err == nil && authorized
-				}
-				return true
-			}
-			return false
-		},
-		Unauthorized: func(c *gin.Context, code int, message string) {
-			c.JSON(code, HttpStatusResponse{
-				Code:    fmt.Sprintf("%d", code),
-				Message: message,
-			})
-		},
-		TokenLookup:   "header: Authorization, query: token, cookie: jwt",
-		TokenHeadName: "Bearer",
-		TimeFunc:      time.Now,
-	})
-
-	if err != nil {
-		log.Fatal("JWT Error:" + err.Error())
-	}
-
-	router.POST((*authInfo).LoginEndPoint, authMiddleware.LoginHandler)
-
-	return authMiddleware
 }
